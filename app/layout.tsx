1d567dc68c5edabad90cb983ccfe41c0765693a4
ctxnlubtxn <101271150+ctxnlubtxn@users.noreply.github.com>
2024-03-23 22:13:57 +0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
 
         <ThemeProvider
           attribute="class"
-          defaultTheme="light"
+          defaultTheme="white"
           enableSystem
           disableTransitionOnChange
         ><NavMenu></NavMenu>
