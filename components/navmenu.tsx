fa0871979c79628e96c11b6d06be294dee8cb157
ctxnlubtxn <101271150+ctxnlubtxn@users.noreply.github.com>
2024-03-23 22:53:26 +0700

    Update navmenu.tsx

--- components/navmenu.tsx
+++ components/navmenu.tsx
@@ -5,7 +5,6 @@ import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigge
 import { BsMoonStarsFill, BsSunFill } from 'react-icons/bs';
 import { useTheme } from 'next-themes';
 import * as React from 'react';
-import { Button } from '@/components/ui/button';
 
 export default function NavMenu() {
     const { setTheme } = useTheme();
@@ -17,11 +16,7 @@ export default function NavMenu() {
             <div className="hidden md:flex items-center">
                 <DropdownMenu>
                     <DropdownMenuTrigger asChild>
-                        <Button variant="outline" size="icon" id="changeThemeMobile">
-                            <BsSunFill className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-                            <BsMoonStarsFill className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-                            <span className="sr-only">Toggle theme</span>
-                        </Button>
+                        <span className="sr-only">Toggle theme</span>
                     </DropdownMenuTrigger>
                     <DropdownMenuContent align="end">
                         {/* <DropdownMenuItem onClick={() => setTheme('light')}>Light</DropdownMenuItem>
